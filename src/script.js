988c79b8195fe1cab708d78f8dff186f17371a5d
khushpatel00 <patelxkhush@gmail.com>
2025-09-04 22:18:04 +0530

    orgainzed gsap improved nav animation

--- src/script.js
+++ src/script.js
@@ -1,22 +1,2 @@
 let nav = document.querySelector('nav');
 let isNavBarOpen = false;
-function navTrigger(){
-    if(isNavBarOpen){
-        gsap.to(nav,{
-            x: "60%",
-            duration: 1,
-            opacity:0,
-            ease: "expo.out"
-        })
-        isNavBarOpen = false;
-    }else{
-        gsap.to(nav,{
-            x: 0,
-            duration: 1,
-            opacity:1,
-            ease: "expo.out"
-        })
-        isNavBarOpen = true;
-    }
-
-}
