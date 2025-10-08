de53a5cb23ca1ecf8da6c91d6023a011a240080a
khushpatel00 <patelxkhush@gmail.com>
2025-10-08 10:19:32 +0530

    added: fluid cursor sticking to element bug: element properties not aquired

--- src/script.js
+++ src/script.js
@@ -1,14 +1,19 @@
-    // ["#000000","#00020b","#000315","#000420","#00052a","#000735","#00083f","#000949","#000a53"]
+    "use strict";
+   
+  // ["#000000","#00020b","#000315","#000420","#00052a","#000735","#00083f","#000949","#000a53"]
     let nav = document.querySelector('nav');
     let isNavBarOpen = false;
     let banner = document.getElementById('banner');
     let fluidCursor = document.getElementById('fluidCursorElement');
     let fluidCursorContent = document.getElementById('fluidCursorContent');
     let scalerObject = document.querySelectorAll('.scalerObject');
-
+    let vertDivider1 = document.querySelector('#vertDivider1')
+    let vertDivider2 = document.querySelector('#vertDivider2')
+    let links = document.querySelectorAll('a');
+    
     let mouseX = 0; // position to be animated
     let mouseY = 0;
-    let targetX = 0; //actual position
+    let targetX = 0; //actual position      
     let targetY = 0;
     let ease = 0.1; 
 
